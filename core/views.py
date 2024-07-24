6465075723acab3aa7cb78de350721beebbfb1eb
trudyscamp <viictorcabraal@outlook.com>
2024-07-24 15:34:33 -0300

    Projeto_quase_finalizado

--- core/views.py
+++ core/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
-
+from core.models import Produto
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.template import loader 
 def index(request):
+    
+    produtos = Produto.objects.all()
     conteudos = {
-        'curso' : 'Programação em Python - Django Framework'
-    }
+        'curso' : 'Programação em Python - Django Framework',
+        'produto' : produtos
+    }   
     return render(request, 'index.html', conteudos)
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request , id):
+    
+    prod = get_object_or_404(Produto,id=id)
+    conteudos = {
+        'produto' : prod
+    }
+    return render(request , 'produto.html' , conteudos)
+
+
+def error404(request,exception):
+    template = loader.get_template('404.html')
+    
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8' , status=404)
+def error500(request):
+    template = loader.get_template('500.html')
+    
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8' , status=500)
---
+++ staticfiles/js/script.js
@@ -0,0 +1,4 @@
+// Função para abrir uma janela de alerta!
+function teste(){
+    alert("Botão está funcionando!");
+}
